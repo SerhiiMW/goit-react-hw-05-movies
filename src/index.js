6bf4957c22027af884345141e82d1ec00a3ba7e5
SerhiiMW <137871447+SerhiiMW@users.noreply.github.com>
2024-01-31 10:10:08 +0200

    homework-5

--- src/index.js
+++ src/index.js
@@ -6,7 +6,7 @@ import { App } from 'App';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <BrowserRouter basename='https://serhiimw.github.io/'>
+    <BrowserRouter basename='/goit-react-hw-05-movies'>
       <App />
     </BrowserRouter>
   </React.StrictMode>
